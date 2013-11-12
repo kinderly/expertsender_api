1ec40b845f3df6a1b8168d967e1cd6027f6cd5bc
Yury Kotov <bairkan@gmail.com>
2013-11-12 11:30:42 +0400

    Snippet value can be wrapped to CDATA

--- lib/expertsender_api/email/snippet.rb
+++ lib/expertsender_api/email/snippet.rb
@@ -1,17 +1,22 @@
 module ExpertSenderApi::Email
   class Snippet
-    attr_accessor :name, :value
+    attr_accessor :name, :value, :cdata
 
-    def initialize(name: nil, value: nil)
+    def initialize(name: nil, value: nil, cdata: true)
       @name = name
       @value = value
+      @cdata = cdata
     end
 
     def insert_to(xml)
       if !name.empty? and !value.empty?
         xml.Snippet {
           xml.Name name
-          xml.Value value
+          if cdata
+            xml.Value { xml.cdata value }
+          else
+            xml.Value value
+          end
         }
       end
     end
